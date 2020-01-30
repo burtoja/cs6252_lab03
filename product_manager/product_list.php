a6bfa2cbb29982babb2d60a032e1c75ba5cc5229
J. Allen Burton <jburton6@my.westga.edu>
2020-01-29 19:40:11 -0500

    Added edit button to product manager page (not functional yet)

--- product_manager/product_list.php
+++ product_manager/product_list.php
@@ -28,21 +28,35 @@
                 <th>Name</th>
                 <th class="right">Price</th>
                 <th>&nbsp;</th>
+                <th>&nbsp;</th>
             </tr>
             <?php foreach ($products as $product) : ?>
             <tr>
                 <td><?php echo $product['productCode']; ?></td>
                 <td><?php echo $product['productName']; ?></td>
                 <td class="right"><?php echo $product['listPrice']; ?></td>
-                <td><form action="." method="post">
-                    <input type="hidden" name="action"
-                           value="delete_product">
-                    <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
-                    <input type="hidden" name="category_id"
-                           value="<?php echo $product['categoryID']; ?>">
-                    <input type="submit" value="Delete">
-                </form></td>
+                <td>
+                	<form action="." method="post">
+	                    <input type="hidden" name="action"
+	                           value="delete_product">
+	                    <input type="hidden" name="product_id"
+	                           value="<?php echo $product['productID']; ?>">
+	                    <input type="hidden" name="category_id"
+	                           value="<?php echo $product['categoryID']; ?>">
+	                    <input type="submit" value="Delete">
+                	</form>
+                </td>
+                <td>
+                	<form action="." method="post">
+	                    <input type="hidden" name="action"
+	                           value="edit_product">
+	                    <input type="hidden" name="product_id"
+	                           value="<?php echo $product['productID']; ?>">
+	                    <input type="hidden" name="category_id"
+	                           value="<?php echo $product['categoryID']; ?>">
+	                    <input type="submit" value="Edit">
+                	</form>
+                </td>
             </tr>
             <?php endforeach; ?>
         </table>
